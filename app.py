831e51a7e7214fb8ab9533138adfbb33767fc219
MeherRushi <sudharushi0@gmail.com>
2025-05-20 10:01:11 +0300

    modify app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 # app.py
 from flask import Flask, request
-
+from http import HTTPStatus
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def hello():
-    print("request data", request.data)
-    return "Hello, HTTPS World!"
+    request_data = request.data.decode('utf-8')
+    print("request data:", request_data)
+    return request_data, HTTPStatus.OK
 
 if __name__ == "__main__":
     app.run()
